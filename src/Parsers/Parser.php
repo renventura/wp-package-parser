3b38c69d82f3ae4b225dd25d7c3a3e34b3683a22
Ren Ventura <rv@renventura.com>
2024-05-25 22:29:40 -0400

    Added PHPCS

--- src/Parsers/Parser.php
+++ src/Parsers/Parser.php
@@ -1,65 +1,69 @@
 <?php
+
 namespace RenVentura\WPPackageParser\Parsers;
+
 use Parsedown;
 
 /**
  * Parser abstraction.
  */
-abstract class Parser {
+abstract class Parser
+{
+    /**
+     * Header map.
+     *
+     * @var array<string, string>
+     */
+    protected $headerMap = array();
 
-	/**
-	 * Header map.
-	 *
-	 * @var array<string, string>
-	 */
-	protected $headerMap = array();
+    /**
+     * Parse the file contents to retrieve its metadata.
+     *
+     * Searches for metadata for a file, such as a plugin or theme.  Each piece of
+     * metadata must be on its own line. For a field spanning multiple lines, it
+     * must not have any newlines or only parts of it will be displayed.
+     *
+     * @param string $fileContents File contents. Can be safely truncated to 8kiB as that's all WP itself scans.
+     *
+     * @return array<string, string>
+     */
+    protected function parseHeaders(string $fileContents): array
+    {
+        $headers   = array();
+        $headerMap = $this->headerMap;
 
-	/**
-	 * Parse the file contents to retrieve its metadata.
-	 *
-	 * Searches for metadata for a file, such as a plugin or theme.  Each piece of
-	 * metadata must be on its own line. For a field spanning multiple lines, it
-	 * must not have any newlines or only parts of it will be displayed.
-	 *
-	 * @param string $fileContents File contents. Can be safely truncated to 8kiB as that's all WP itself scans.
-	 *
-	 * @return array<string, string>
-	 */
-	protected function parseHeaders( string $fileContents ) : array {
-		$headers   = array();
-		$headerMap = $this->headerMap;
+        // Support systems that use CR as a line ending.
+        $fileContents = str_replace("\r", "\n", $fileContents);
 
-		// Support systems that use CR as a line ending.
-		$fileContents = str_replace( "\r", "\n", $fileContents );
+        foreach ($headerMap as $field => $prettyName) {
+            $found = preg_match('/^[ \t\/*#@]*' . preg_quote($prettyName, '/') . ':(.*)$/mi', $fileContents, $matches);
+            if (( $found > 0 ) && ! empty($matches[1])) {
+                // Strip comment markers and closing PHP tags.
+                $value = trim(preg_replace("/\s*(?:\*\/|\?>).*/", '', $matches[1]));
+                $headers[ $field ] = $value;
+            } else {
+                $headers[ $field ] = '';
+            }
+        }
 
-		foreach ( $headerMap as $field => $prettyName ) {
-			$found = preg_match( '/^[ \t\/*#@]*' . preg_quote( $prettyName, '/' ) . ':(.*)$/mi', $fileContents, $matches );
-			if ( ( $found > 0 ) && ! empty( $matches[1] ) ) {
-				// Strip comment markers and closing PHP tags.
-				$value = trim( preg_replace( "/\s*(?:\*\/|\?>).*/", '', $matches[1] ) );
-				$headers[ $field ] = $value;
-			} else {
-				$headers[ $field ] = '';
-			}
-		}
-
-		return $headers;
-	}
+        return $headers;
+    }
 
 
-	/**
-	 * Transform Markdown markup to HTML.
-	 *
-	 * Tries (in vain) to emulate the transformation that WordPress.org applies to readme.txt files.
-	 *
-	 * @param string $text
-	 *
-	 * @return string
-	 */
-	protected function applyMarkdown( string $text ) : string {
-		// The WP standard for readme files uses some custom markup, like "= H4 headers ="
-		$text     = preg_replace( '@^\s*=\s*(.+?)\s*=\s*$@m', "<h4>$1</h4>\n", $text );
-		$markdown = new Parsedown();
-		return $markdown->parse( $text );
-	}
+    /**
+     * Transform Markdown markup to HTML.
+     *
+     * Tries (in vain) to emulate the transformation that WordPress.org applies to readme.txt files.
+     *
+     * @param string $text
+     *
+     * @return string
+     */
+    protected function applyMarkdown(string $text): string
+    {
+        // The WP standard for readme files uses some custom markup, like "= H4 headers ="
+        $text     = preg_replace('@^\s*=\s*(.+?)\s*=\s*$@m', "<h4>$1</h4>\n", $text);
+        $markdown = new Parsedown();
+        return $markdown->parse($text);
+    }
 }
--- src/Parsers/ThemeParser.php
+++ src/Parsers/ThemeParser.php
@@ -1,47 +1,49 @@
 <?php
+
 namespace RenVentura\WPPackageParser\Parsers;
 
 /**
  * Class for parsing a WordPress theme.
  */
-class ThemeParser extends Parser {
+class ThemeParser extends Parser
+{
+    /**
+     * Header map.
+     *
+     * @var array<string, string>
+     */
+    protected $headerMap = array(
+        'name'        => 'Theme Name',
+        'theme_uri'   => 'Theme URI',
+        'description' => 'Description',
+        'author'      => 'Author',
+        'author_uri'  => 'Author URI',
+        'version'     => 'Version',
+        'template'    => 'Template',
+        'status'      => 'Status',
+        'tags'        => 'Tags',
+        'text_domain' => 'Text Domain',
+        'domain_path' => 'Domain Path',
+    );
 
-	/**
-	 * Header map.
-	 *
-	 * @var array<string, string>
-	 */
-	protected $headerMap = array(
-		'name'        => 'Theme Name',
-		'theme_uri'   => 'Theme URI',
-		'description' => 'Description',
-		'author'      => 'Author',
-		'author_uri'  => 'Author URI',
-		'version'     => 'Version',
-		'template'    => 'Template',
-		'status'      => 'Status',
-		'tags'        => 'Tags',
-		'text_domain' => 'Text Domain',
-		'domain_path' => 'Domain Path',
-	);
+    /**
+     * Parse style.css file.
+     *
+     * @param string $fileContents Contents of style.css file.
+     *
+     * @return null|array<string, string>
+     */
+    public function parseStyle(string $fileContents): null|array
+    {
 
-	/**
-	 * Parse style.css file.
-	 *
-	 * @param string $fileContents Contents of style.css file.
-	 *
-	 * @return null|array<string, string>
-	 */
-	public function parseStyle( string $fileContents ) : null|array {
+        $headers = $this->parseHeaders($fileContents);
+        $headers['tags'] = array_filter(array_map('trim', explode(',', strip_tags($headers['tags']))));
 
-        $headers = $this->parseHeaders( $fileContents );
-		$headers['tags'] = array_filter( array_map( 'trim', explode( ',', strip_tags( $headers['tags'] ) ) ) );
+        // If it doesn't have a name, it's probably not a valid theme.
+        if (empty($headers['name'])) {
+            return null;
+        }
 
-		// If it doesn't have a name, it's probably not a valid theme.
-		if ( empty( $headers['name'] ) ) {
-			return null;
-		}
-
-		return $headers;
-	}
+        return $headers;
+    }
 }
